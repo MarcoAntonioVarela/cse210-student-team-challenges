8a731195ddf512917b6f43419a1700962fbcbf63
Anna Merrill <mer20014@byui.edu>
2021-10-20 11:05:18 -0600

    player class

---
+++ 06-mastermind/mastermind/game/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__ (self, name):
+        self._name = name
+        self._move = None
+
+    def get_name(self):
+        return self._name
+    
+    def get_move(self):
+        return self._move
+
+    def set_move(self, move):
+        self._move = move
