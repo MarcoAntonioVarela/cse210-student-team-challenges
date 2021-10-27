2a07cf5e0226894701ed6b6842e894433b953167
Brendan Jessen <jes20003@byui.edu>
2021-10-27 11:05:04 -0600

    Updated director class

--- 07-speed/speed/game/director.py
+++ 07-speed/speed/game/director.py
@@ -1,3 +1,24 @@
-class Director():
-    def __init__(self):
+from time import sleep
+import os
+os.environ['asciimatics_BIN_PATH'] = '.'
+import asciimatics
+from game import constants
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+       
+    """
+
+    def __init__(self, input_service, output_service):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         pass
