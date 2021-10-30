059d03ca2bb983587fca2e0951895d28df02285d
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-10-30 13:36:59 -0600

    Creating buffer.py

---
+++ 07-speed/speed/game/buffer.py
@@ -0,0 +1,20 @@
+from game import constants
+from game.actor import Actor
+
+class Buffer(Actor):
+
+    def __init__(self):
+        super().__init__()
+    
+    def add_char(self,char):
+
+    def get_chars(self):
+
+    def compare (self,string):
+
+    def reset_buffer(self):
+
+    self.chars=''
+    self.set_text(f'Bufer:{self.chars})')
+
+    
