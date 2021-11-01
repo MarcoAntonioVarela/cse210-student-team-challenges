ead431c6de1a5b933b7fef67e26cc1be50f672f0
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-10-31 20:38:38 -0600

    Updating buffer

--- 07-speed/speed/game/buffer.py
+++ 07-speed/speed/game/buffer.py
@@ -2,19 +2,24 @@ from game import constants
 from game.actor import Actor
 
 class Buffer(Actor):
-
     def __init__(self):
         super().__init__()
-    
-    def add_char(self,char):
+        self.chars = ''
+        self._position._y = constants.MAX_Y - 1
+
+    def add_char(self, char):
+        self.chars += char
+        self.set_text(f'Buffer:{self.chars}')
 
     def get_chars(self):
+        return self.chars
 
-    def compare (self,string):
+    def compare(self, string):
+        if (string in self.chars):
+            return True
+        else:
+            return False
 
     def reset_buffer(self):
-
-    self.chars=''
-    self.set_text(f'Bufer:{self.chars})')
-
-    
+        self.chars = ''
+        self.set_text(f'Buffer:{self.chars}')
