35d42709dc9660b729538a2f811fce6691a17ce9
Marco Varela <85329426+MarcoAntonioVarela@users.noreply.github.com>
2021-11-05 08:04:47 -0600

    I updated this code by following Brother Burton's template

--- 07-speed/speed/game/constants.py
+++ 07-speed/speed/game/constants.py
@@ -1,9 +1,18 @@
+#I updated this code by following Brother Burton's template
+
 import os
 
-MAX_X = 600
+MAX_X = 800
 MAX_Y = 400
-FRAME_LENGTH = 0.08
+FRAME_RATE = 30
+
+DEFAULT_SQUARE_LENGTH = 20
+DEFAULT_FONT_SIZE = 20
+DEFAULT_TEXT_OFFSET = 5
+
+MAX_SPEED = 5
+WORD_GENERATION_RATE = 0.02
+
 STARTING_WORDS = 5
-DIFFICULTY = 1
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
